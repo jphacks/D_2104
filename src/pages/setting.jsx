3dcc09020ef97be561f3306b3c31f70d324e08f0
nac-39 <83295760+nac-39@users.noreply.github.com>
2021-10-27 00:32:02 +0900

    setting.jsxから不要な部分を削除

--- src/pages/setting.jsx
+++ src/pages/setting.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 const Setting = () => {
-  const { addon } = require("bindings")("Visualize_Sounds_Core_addon");
-  console.log(addon)
-  console.log(addon.RegisterSource)
-  const r = addon.RegisterSource("", "", "");
-  r.then((resp) => {
-    const data = resp
-    console.log(data)
-  });
+  // const { addon } = require("bindings")("Visualize_Sounds_Core_addon");
+  // console.log(addon)
+  // console.log(addon.RegisterSource)
+  // const r = addon.RegisterSource("", "", "");
+  // r.then((resp) => {
+  //   const data = resp
+  //   console.log(data)
+  // });
   return (
     <div>
       <p>設定ページです</p>
