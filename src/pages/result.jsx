58b776d6ae68c70a459a1031debfe24d021c17c9
morita-koki <morita82007koki@gmail.com>
2021-10-27 21:07:01 +0900

    いい感じにしたよ！

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -25,22 +25,30 @@ const ShowNodeData = ({ nodeData }) => { //nodeの情報を書く
 };
 
 const Result = () => {
+  const openFolder = () => {
+    const { shell } = window.require('electron');
+    const os = window.require('os');
+    //一旦はどのosでも動くようにhomedirを開く。
+    shell.showItemInFolder(os.homedir());
+    //osによってファイルパスの扱いが違うけど、受け取ったパスをそのまま使って大丈夫そう
+    // shell.showItemInFolder("C:\\Users\\morit\\Pictures\\My Cloud Samples\\sample01.jpg");
+  };
+
   const [data, setData] = useState({ nodes: new vis.DataSet([]) });
 
-  // データの検索処理
-  useEffect(() => {
+  const search = () => { //dataを更新→useEffectの依存値にdataを加える
     const response = addon.FindSimilarAudioFromNode([{}], [
       [{}]
     ]);
-  
+
     response.then((nodes) => {
       console.log(nodes)
       const nodesFeatures = nodes.map((datum) => {
         return parseFeature(datum.feature, MAX_DIMENSION)
       })
-  
+
       const adjustedData = reduceDimensions(nodesFeatures, 2);
-  
+
       return nodes.map((node, idx) => {
         return { id: idx, x: adjustedData[0][idx], y: adjustedData[1][idx], title: node.path }
       })
@@ -49,30 +57,14 @@ const Result = () => {
       console.log(dataSet)
       setData({ nodes: dataSet })
     })
-  }, [])
+  };
 
-  // console.log(adData);
-  
-  // const nodes = new vis.DataSet([
-  //   { id: 1, label: 'A', x: 20, y: 180, title: "/asset/music1.wav" },
-  //   { id: 2, label: 'B', x: 40, y: 200, title: "/asset/music2.wav" },
-  //   { id: 3, label: 'C', x: 60, y: 10, title: "/asset/music3.wav" },
-  //   { id: 4, label: 'D', x: 80, y: 150, title: "/asset/music4.wav" },
-  //   { id: 5, label: 'E', x: 100, y: 300, title: "/asset/music5.wav" },
-  //   { id: 6, label: 'F', x: 120, y: 100, title: "/asset/music6.wav" },
-  //   { id: 7, label: 'G', x: 150, y: 250, title: "/asset/music7.wav" },
-  //   { id: 8, label: 'H', x: 180, y: 50, title: "/asset/music8.wav" },
-  // ]);
-  // const data = {
-  //   nodes: nodes,
-  // };
-  
   const rootNode = { id: 1, label: 'A', x: 20, y: 180, title: "/asset/music1.wav" };
   const [clickedNode, setClickedNode] = useState(null);
 
   const options = {
-    height: '500px',
-    width: '800px',
+    height: '320px',
+    width: '480px',
     physics: {
       enabled: true,
       maxVelocity: 50,
@@ -93,20 +85,18 @@ const Result = () => {
     });
   };
 
-  const openFolder = () => {
-    const { shell } = window.require('electron');
-    const os = window.require('os');
-    //一旦はどのosでも動くようにhomedirを開く。
-    shell.showItemInFolder(os.homedir());
-    //osによってファイルパスの扱いが違うけど、受け取ったパスをそのまま使って大丈夫そう
-    // shell.showItemInFolder("C:\\Users\\morit\\Pictures\\My Cloud Samples\\sample01.jpg");
-  };
-
   // ###########################
   // ここレンダリング問題残ってる
   // ###########################
-  // useEffect(() => draw(), []);
-  draw()
+
+  useEffect(() => search(), []); //初回render後に一度だけ呼ぶ（これがないと初めにグラフが表示されない
+  //dataはsearch()に依存
+  //ClickedNodeはdraw()に依存
+  //流れ
+  //再検索ボタンが押される→search()が呼ばれる
+  //→dataが更新→draw()が呼ばれる
+  //→ClickedNodeが更新→reactがいい感じにrenderしてくれる
+  useEffect(() => draw(), [data]);
 
   return (
     <div style={{ background: "#191E2B" }}>
@@ -117,7 +107,7 @@ const Result = () => {
           <p>----------------------</p>
           <ShowNodeData nodeData={clickedNode} />
           <Button onClick={openFolder} style={{ background: "#5500BB" }}>参照</Button>
-          <Button style={{ background: "#5500BB" }}>再検索</Button>
+          <Button onClick={search} style={{ background: "#5500BB" }}>再検索</Button>
         </div>
       </div >
       <Link to="/serch">検索ページへ</Link><br />
